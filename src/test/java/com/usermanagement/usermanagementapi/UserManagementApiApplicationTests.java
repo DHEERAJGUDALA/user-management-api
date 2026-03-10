7bacc09faddcd4f581d579558a0136e9cbb68c88
DHEERAJGUDALA <dheerajgudala1001lovesai200@gmail.com>
2026-03-10 18:13:22 +0530

    "Initial Commit"

---
+++ src/test/java/com/usermanagement/usermanagementapi/UserManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usermanagement.usermanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
