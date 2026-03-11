318561a6e42a5999a1d7a3ec5674d7146876480b
DHEERAJGUDALA <dheerajgudala1001lovesai200@gmail.com>
2026-03-11 22:30:15 +0530

    ""

--- src/main/java/com/usermanagement/UserManagementApiApplication.java
+++ src/main/java/com/usermanagement/UserManagementApiApplication.java
@@ -1,4 +1,4 @@
-package com.usermanagement.usermanagementapi;
+package com.usermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
