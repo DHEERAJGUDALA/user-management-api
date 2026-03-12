beb926beb621b87a4d3fbd8d45f6b809ff665e36
DHEERAJGUDALA <dheerajgudala1001lovesai200@gmail.com>
2026-03-12 16:17:49 +0530

    "User Request Handler"

---
+++ src/main/java/com/usermanagement/dto/CreateUserRequest.java
@@ -0,0 +1,25 @@
+package com.usermanagement.dto;
+import com.usermanagement.entity.User;
+import jakarta.validation.constraints.*;
+import lombok.*;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CreateUserRequest {
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
+    private String firstName;
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
+    private String lastName;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+    @NotNull(message = "Role is required")
+    private User.Role role;
+}
