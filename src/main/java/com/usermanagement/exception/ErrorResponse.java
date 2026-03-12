a379a69ad03509056393b47626d5349f708d560e
DHEERAJGUDALA <dheerajgudala1001lovesai200@gmail.com>
2026-03-12 16:18:31 +0530

    "Error Response Exception"

---
+++ src/main/java/com/usermanagement/exception/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.usermanagement.exception;
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.List;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorResponse {
+    private int status;
+    private String message;
+    private LocalDateTime timestamp;
+    private List<String> errors; // for validation errors — multiple at once
+}
