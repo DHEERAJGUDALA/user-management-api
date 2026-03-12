c06157bd9974c1f74db64067c84af6903d6370b2
DHEERAJGUDALA <dheerajgudala1001lovesai200@gmail.com>
2026-03-12 16:18:12 +0530

    "Email Exception"

---
+++ src/main/java/com/usermanagement/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,6 @@
+package com.usermanagement.exception;
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String email) {
+        super("Email already exists: " + email);
+    }
+}
