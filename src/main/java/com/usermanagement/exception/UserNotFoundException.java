b2e9ac1d2d0520c13ad691f0f4a10e729c5f1542
DHEERAJGUDALA <dheerajgudala1001lovesai200@gmail.com>
2026-03-12 16:19:54 +0530

    "User Exceptions"

---
+++ src/main/java/com/usermanagement/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.usermanagement.exception;
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(Long id) {
+        super("User not found with id: " + id);
+    }
+    public UserNotFoundException(String email) {
+        super("User not found with email: " + email);
+    }
+}
